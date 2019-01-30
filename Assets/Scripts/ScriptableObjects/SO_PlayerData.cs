e0cb5e78253005107e30ffc5037463d1c97a5758
Harish Kumar <harishsghp@gmail.com>
2019-01-30 00:29:09 -0800

    Initial commit

---
+++ Assets/Scripts/ScriptableObjects/SO_PlayerData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerData", menuName = "BomberMan/PlayerData")]
+
+public class SO_PlayerData : ScriptableObject
+{
+	[Header("Key Bindings")]
+	public KeyCode UpKey = KeyCode.W;
+	public KeyCode DownKey = KeyCode.S;
+	public KeyCode RightKey = KeyCode.D;
+	public KeyCode LeftKey = KeyCode.A;
+
+	[Header("Player Identification")]
+	public Color PlayerColorShade = Color.blue;
+}
